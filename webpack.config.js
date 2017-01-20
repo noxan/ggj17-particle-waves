dfe91d6962397eee056de8240eaf794e07bf8c5c
Richard <hello@noxan.net>
2017-01-21 00:14:19 +0100

    add webpack image loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,13 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ],
+      },
     ],
   },
   resolve: {
